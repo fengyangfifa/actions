72c0cb9aafae2b7769280ee209fbd2cafe7f657b
fengyangfifa <2314034614@qq.com>
2024-08-27 22:25:09 +0800

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+import "./app.css";
+import "./app.scss";
+import web from "./assets/web.webp";
+import testTxt from "./test.txt";
+import JSONTxt from "./test.json";
+import Hello from "./components/hello";
+
+function App() {
+  const [count, setCount] = useState(1);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  useEffect(() => {
+    console.log(count);
+  }, [count]);
+
+  return (
+    <div>
+      hello react
+      <p className="count">{count}</p>
+      <img src={web} alt="web" />
+      <p>{testTxt}</p>
+      <p>{JSONTxt.name}</p>
+      <button className="click" onClick={handleClick}>
+        click
+      </button>
+      <Hello name={""} />
+    </div>
+  );
+}
+
+export default App;
